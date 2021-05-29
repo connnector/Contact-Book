2a5310fd3837b201860b551fa46c59159e4ef05a
XshubhamX <shubham.11901502@lpu.in>
2021-05-29 12:10:59 +0530

    base setup
    server setrup

---
+++ middlewares/isAuth.js
@@ -0,0 +1,52 @@
+import jwt from "jsonwebtoken";
+
+const isAuth = (req) => {
+  // This checks to see if there is an authorization field within the incoming request
+  const authHeader = req.headers.authorization;
+  let isAuth = false;
+
+  // if there is no token
+  if (!authHeader) {
+    return {
+      isAuth,
+    };
+  }
+
+  // format of request sent will be Bearer tokenvalue
+  // this splits it into two values bearer and the token
+  const token = authHeader.split(" ")[1];
+
+  // if the token is null or an empty string
+  if (!token || token === "") {
+    return {
+      isAuth,
+    };
+  }
+  // console.log(token);
+
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    if (!decodedToken) {
+      return {
+        isAuth,
+      };
+    }
+  } catch (e) {
+    return {
+      isAuth,
+    };
+  }
+
+  // shows the user is an authenticated user
+  // pulls data off the token
+  const { id } = decodedToken;
+  isAuth = true;
+
+  return {
+    isAuth,
+    id,
+  };
+};
+
+export { isAuth as default };
